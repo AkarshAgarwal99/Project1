52b523797be46fc07ae10e24549df8e393ce5fb5
AKARSH AGARWAL <48346472+AkarshAgarwal99@users.noreply.github.com>
2023-02-14 17:22:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import boto3
 from flask import Flask, request, render_template, Response
 import PyPDF2
+from flask_cors import CORS, cross_origin
+import os
 
 app = Flask(__name__)
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS")
+
+cors = CORS(app, resources={"/*": {"origins": "*"}}, support_credentials=True)
 polly = boto3.client("polly")
 
 @app.route("/")
 def index():
     voices = polly.describe_voices()['Voices']
-    return render_template("index.html", voices=voices)
+    list_of_voices = { "voices": voices}
+    return list_of_voices
 
 @app.route("/polly", methods=['POST'])
 def generate_audio_from_pdf():
@@ -31,6 +37,20 @@ def generate_audio_from_pdf():
         mimetype="audio/mpeg",
         headers={"Content-Disposition": "attachment;filename=sound.mp3"}
     )
+@app.route('/translate', methods=['POST'])
+def translate():
+    text = request.form['text']
+    source_lang = request.form['source_lang']
+    target_lang = request.form['target_lang']
+
+    translate = boto3.client('translate')
+    response = translate.translate_text(Text=text, SourceLanguageCode=source_lang, TargetLanguageCode=target_lang)
+    output = { "input_text": text,
+              "translation_text": response['TranslatedText'],
+              "source_lang": source_lang,
+              "target_lang": target_lang
+              }
+    return output
 
 if __name__ == "__main__":
     app.run(debug=True)
